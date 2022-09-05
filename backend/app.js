58c51b152f7f993607059145e55b91f9e6de3284
Michael Zhu <mbzhu1@gmail.com>
2022-09-05 14:11:22 -0700

    add replicate js

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,11 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.post("/", (req, res) => {
+  res.send("Hello World!");
+  console.log(req.body);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
