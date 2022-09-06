28105b4856770267a10d42e7d0b180f2be700397
Michael Zhu <mbzhu1@gmail.com>
2022-09-05 18:07:05 -0700

    start game page scaffold done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,12 @@
-import { useEffect, useRef, useState } from 'react'
+import { useRef } from 'react'
 import './App.css'
-import ImageGrid from './components/ImageGrid'
-import StartGame from './components/StartGame'
 import { useUsername } from './hooks/useUsername'
 
 function App() {
   const [username, setUsername] = useUsername()
   const usernameRef = useRef<HTMLInputElement>(null)
+  const gameIdRef = useRef<HTMLInputElement>(null)
+
   const onSetUsername = () => {
     if (usernameRef.current) {
       console.log(usernameRef.current.value)
@@ -31,7 +31,24 @@ function App() {
           <button onClick={onSetUsername}>Set Username</button>
         </>
       ) : (
-        <StartGame />
+        <>
+          <h1>Welcome {username}!</h1>
+          <div className="flex justify-around">
+            <div>
+              <input
+                ref={gameIdRef}
+                className="p-2 my-2 mx-auto block"
+                type="number"
+                name="gameID"
+                placeholder="game id"
+              />
+              <button className="my-2 mx-auto block">Join Game</button>
+            </div>
+
+            <button className="my-2 block">Create Game</button>
+          </div>
+          <button onClick={() => setUsername('')}>Logout</button>
+        </>
       )}
     </div>
   )
