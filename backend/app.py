376fd6f864fc52fa15c1b40f47996a0e1948f40f
Armaan Goel <armaangoel@Armaans-MacBook-Air.local>
2022-09-05 20:58:23 -0700

    cors hack

--- backend/app.py
+++ backend/app.py
@@ -3,8 +3,11 @@ from flask import Flask, request, jsonify
 import replicate
 from db import set_guess
 import db
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
 
 @app.route("/")
 def home():
