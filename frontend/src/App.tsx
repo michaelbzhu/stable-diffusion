fee60c335c863ced91af5516d00a17469626afce
Michael Zhu <mbzhu1@gmail.com>
2022-09-05 17:55:25 -0700

    start game flow

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,38 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 import ImageGrid from './components/ImageGrid'
-import UserGuess from './components/UserGuess'
-import { generateImage } from './utils/generateImage'
-import GuesserView from './views/GuesserView'
-import PrompterView from './views/PrompterView'
+import StartGame from './components/StartGame'
+import { useUsername } from './hooks/useUsername'
 
 function App() {
+  const [username, setUsername] = useUsername()
+  const usernameRef = useRef<HTMLInputElement>(null)
+  const onSetUsername = () => {
+    if (usernameRef.current) {
+      console.log(usernameRef.current.value)
+      setUsername(usernameRef.current.value)
+    } else {
+      setUsername('')
+    }
+  }
+  console.log(username)
   return (
     <div>
       <h1>Stable Diffusion Guesser</h1>
-      <input
-        className="p-2 my-2 mx-auto block"
-        type="text"
-        name="username"
-        placeholder="username"
-      />
-      <input
-        className="p-2 my-2 mx-auto block"
-        type="number"
-        name="gameID"
-        placeholder="game id"
-      />
-      <button className="my-2 mx-auto block">Join Game</button>
-      <button className="my-2 mx-auto block">Create Game</button>
+      {username === '' ? (
+        <>
+          <input
+            ref={usernameRef}
+            className="p-2 my-2 mx-auto block"
+            type="text"
+            name="username"
+            placeholder="username"
+          />
+          <button onClick={onSetUsername}>Set Username</button>
+        </>
+      ) : (
+        <StartGame />
+      )}
     </div>
   )
 }
